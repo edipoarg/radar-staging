ef95b36c1c856c8f3a0e87ee2286ef835c7fc696
anon author <noemail@nothing.null>
2024-08-30 13:52:06 -0300

    68-nuevos-contenidos

--- src/components/QuienesSomos/Definiciones/Definiciones.jsx
+++ src/components/QuienesSomos/Definiciones/Definiciones.jsx
@@ -1,3 +1,28 @@
+/* eslint-disable css-modules/no-unused-class */
+import styles from "../QuienesSomos.module.css";
+import definiciones from "../../../../public/data/definiciones.json";
+
 export default function Definiciones() {
-  return <div>Definiciones</div>;
+  return (
+    <section className={styles.section}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Definiciones</h2>
+        <h4 className={styles.description}>
+          En este punto explicamos lo que entendemos por algunos términos
+          repetitivos que usamos cuando investigamos. Son definiciones propias
+          desarrolladas en base a nuestra experiencia militante. No tienen que
+          ser académicas ni consensuadas, es más que todo para que el público
+          conozca nuestro vocabulario de intervención.
+        </h4>
+      </div>
+      <div className={styles.content}>
+        {definiciones.map((item, index) => (
+          <div key={index}>
+            <h3 className={styles.term}>{item.termino}:</h3>
+            <h5 className={styles.definition}>{item.definicion}</h5>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
--- src/components/QuienesSomos/QuienesSomos.tsx
+++ src/components/QuienesSomos/QuienesSomos.tsx
@@ -1,3 +1,41 @@
+/* eslint-disable css-modules/no-unused-class */
+import { NavLink } from "react-router-dom";
+import styles from "./QuienesSomos.module.css";
+import Navlinks from "../../routes/index";
+
 export default function QuienesSomos() {
-  return <div>Quienes somos</div>;
+  return (
+    <section className={styles.section}>
+      <div className={styles.title}>Quienes somos</div>
+
+      <NavLink
+        to={Navlinks.presentacion}
+        title="presentacion"
+        className={styles.navLink}
+      >
+        <h2 className={styles.heading}>Presentación</h2>
+      </NavLink>
+      <NavLink
+        to={Navlinks.definiciones}
+        title="definiciones"
+        className={styles.navLink}
+      >
+        <h2 className={styles.heading}>Definiciones</h2>
+      </NavLink>
+      <NavLink
+        to={Navlinks.patronDeCasos}
+        title="patronDeCasos"
+        className={styles.navLink}
+      >
+        <h2 className={styles.heading}>Patrón de Casos</h2>
+      </NavLink>
+      <NavLink
+        to={Navlinks.metodologia}
+        title="metodologia"
+        className={styles.navLink}
+      >
+        <h2 className={styles.heading}>Metodología</h2>
+      </NavLink>
+    </section>
+  );
 }
