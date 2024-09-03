02ec2a91009b3d9aa2f65919a4fa2c580aa69604
anon author <noemail@nothing.null>
2024-09-03 11:37:59 -0300

    type all the scripts

--- src/components/QuienesSomos/PatronDeCasos/PatronDeCasos.tsx
+++ src/components/QuienesSomos/PatronDeCasos/PatronDeCasos.tsx
@@ -1,56 +1,42 @@
-/* eslint-disable css-modules/no-unused-class */
-import React, { useEffect, useState } from "react";
-import styles from "../QuienesSomos.module.css"; // Asegúrate de que la ruta sea correcta
-import patronDeCasosData from "../../../../public/data/patronDeCasos.json"; // Ajusta la ruta si es necesario
+import styles from "../QuienesSomos.module.css";
+import { patronDeCasos } from "./patronDeCasos";
 
 export default function PatronDeCasos() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    // Simula la carga de datos desde un archivo JSON
-    setData(patronDeCasosData);
-  }, []);
-
-  if (!data) return <div>Cargando...</div>;
-
   return (
     <section className={styles.section}>
       <div className={styles.header}>
-        <h2 className={styles.title}>{data.title}</h2>
+        <h2 className={styles.title}>{patronDeCasos.title}</h2>
       </div>
 
       <div className={styles.content}>
-        {data.sections.map((section, index) => (
-          <div key={index}>
-            <h3 className={styles.term}>{section.term}</h3>
-            {section.categories ? (
-              section.categories.map((category, catIndex) => (
-                <div key={catIndex}>
-                  <h4 className={styles.subTerm}>{category.subTerm}</h4>
-                  <h5 className={styles.description}>{category.description}</h5>
-                  <ul>
-                    {category.list.map((item, listIndex) => (
-                      <li key={listIndex} className={styles.definition}>
-                        {item}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))
-            ) : (
-              <>
-                <h5 className={styles.description}>{section.description}</h5>
-                <ul>
-                  {section.list.map((item, listIndex) => (
-                    <li key={listIndex} className={styles.definition}>
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-          </div>
-        ))}
+        {Object.entries(patronDeCasos.categories).map(
+          ([categoryName, category]) => {
+            return (
+              <div key={categoryName}>
+                <h3>{categoryName}</h3>
+                {Object.entries(category.terms).map(([termName, termValue]) => (
+                  <div key={termName}>
+                    <h4>{termName}</h4>
+                    {termValue.definitions.map((definition) =>
+                      definition.paragraphs.map((paragraph) => (
+                        <h5 key={paragraph} className={styles.description}>
+                          {paragraph}
+                        </h5>
+                      )),
+                    )}
+                    <ul>
+                      {termValue.members.map((item) => (
+                        <li key={item} className={styles.definition}>
+                          {item}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            );
+          },
+        )}
       </div>
     </section>
   );
